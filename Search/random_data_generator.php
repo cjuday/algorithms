8978199e9c2fe06fcf1246ce6d8b2a4901f6574f
cjuday <cjuday88@gmail.com>
2022-08-09 13:45:26 +0600

    Random Number Generator

--- Search/random_data_generator.php
+++ Search/random_data_generator.php
@@ -1,19 +1,18 @@
 <?php
 
-$myfile = fopen("data.txt","w");
-$total_number = 20;
-$range = 500;
+$myfile = fopen("data.txt","w"); //creating file using the opening file function (fopen())
+$total_number = 20; //total number which will be generated
+$range = 500; //maximum range of random number
 
-if($myfile){
+if($myfile){ //check if the file is created or not
     $i = $total_number;
-    while($i>=1){
-        $rand = rand(1,$range);
-        fwrite($myfile, $rand."\n");
-        $i--;
-    }
-    echo "Random number generated!";
-}
+    while($i>=1){ //loop for iteration
+        $rand = rand(1,$range); //random number generator
+        fwrite($myfile, $rand."\n"); //writing the random number to file
+        $i--; //decreasing loop count
+    } //end loop
+    echo "Random number generated!"; //confirmation message
+} //end file opening check
 
-fclose($myfile);
-
+fclose($myfile); //closing the file after end of writing
 ?>
